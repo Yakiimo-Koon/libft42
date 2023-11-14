b43e1546999a46545f0c5911b1ef56c8ab0e717d
Yakiimo <82185133+Yakiimo-Koon@users.noreply.github.com>
2023-11-13 16:32:51 -0800

    Add files via upload

---
+++ ft_memmove.c
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevin <kevin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:50:15 by kevin             #+#    #+#             */
+/*   Updated: 2023/11/13 16:26:36 by kevin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t size)
+{
+	if (dest == src || size == 0)
+		return (dest);
+	if (dest < src || dest >= (char*)src + size)
+	{
+		char *d = (char*)dest;
+		const char *s = (const char*)src;
+
+		while (size > 0)
+		{
+			*d = *s;
+			d++;
+			s++;
+			size--;
+		}
+	}
+	else
+	{
+		char *d = (char*)dest + (size - 1);
+		const char *s = (const char*)src + (size - 1);
+
+		while (size > 0)
+		{
+			d--;
+			s--;
+			size--;
+		}
+	}
+	return (dest);
+}
+
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    // Test with non-overlapping regions
+    char dest1[20];
+    const char src1[] = "Hello, memmove!";
+    ft_memmove(dest1, src1, sizeof(src1));
+
+    // Test with overlapping regions
+    char dest2[20];
+    const char src2[] = "Hello, memmove!";
+    ft_memmove(dest2 + 7, src2, 5);
+
+    // Print the results
+    printf("Test 1 - Non-overlapping regions:\n");
+    printf("Source: %s\n", src1);
+    printf("Destination: %s\n\n", dest1);
+
+    printf("Test 2 - Overlapping regions:\n");
+    printf("Source: %s\n", src2);
+    printf("Destination: %s\n", dest2);
+
+    return 0;
+}
+
+
+
+
+
---
+++ ft_mempcy.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_mempcy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kevin <kevin@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/13 15:33:52 by kevin             #+#    #+#             */
+/*   Updated: 2023/11/13 15:43:10 by kevin            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t size)
+{
+	char *d = (char*)dest;
+	const char *s = (const char*)src;
+
+	while (size > 0)
+	{
+		*d = *s;
+		d++;
+		s++;
+		size--;
+	}
+	return (dest);
+}
+
+ #include <stdio.h>
+int main()
+{
+	const char src[] = "Hello, my memcpy!";
+	char dest[20];
+
+	ft_memcpy(dest, src, sizeof(src));
+	dest[sizeof(src) - 1] = '\0';
+
+	printf("Source: %s\n", src);
+    printf("Destination: %s\n", dest);
+	return 0;
+}
